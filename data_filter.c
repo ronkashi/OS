dc7932626cf6602d7c2b325196cff09547ece887
ronkashi <kashiron1@gmail.com>
2017-04-09 23:19:28 +0300

    Add files via upload

--- data_filter.c
+++ data_filter.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
@@ -8,18 +9,20 @@
 #include <malloc.h>
 #include <stdio.h>
 #define SmallBuf 1024
-#define BigBuf 8192
+#define BigBuf 4096
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
-// print the printable characters of text file
+
+// print the printable (ascii [32,126]) characters of text file
 int main(int argc, char** argv) {
 	// assert first argument filename
 	if (argc != 4) {
-		printf("number of command line arguments isn't fit");
+		printf("number of command line arguments isn't fit\n");
+		return -1;
 	}
 
 	char unit;
-	long Creq = 0;
-	long size;
+	long long Creq = 0;
+	long long size;
 	int i = 0;
     	for(i=0;i<(strlen(argv[1])-1);i++){
             	if(!(argv[1][i] >= 48 && argv[1][i] <= 57))    
@@ -28,8 +31,8 @@ int main(int argc, char** argv) {
 					return -1;
             	}
     	}
-	sscanf(argv[1], "%li%c", &size, &unit); //still a problem size can be big
-	//TODO
+	sscanf(argv[1], "%lld%c", &size, &unit);
+	unit = toupper(unit);
 	switch (unit) {
 	case 'B':
 		Creq = size;
@@ -47,24 +50,19 @@ int main(int argc, char** argv) {
 		printf("Invalid unit\n");
 		return -1;
 	}
-//input file open for RDONLY
+	//input file open for RDONLY
 	int fdInput = open(argv[2], O_RDONLY);
 	if (fdInput < 0) {
 		printf("Error opening Input file: %s\n", strerror( errno));
-		return errno;
-	} else {
-		printf("File is opened. Descriptor %d\n", fdInput);
+		return -1;
 	}
 
-//output file open for WRONLY
-	int fdOut = open(argv[3], O_WRONLY);
-
+	//output file open for WRONLY
+	int fdOut = open(argv[3], O_WRONLY|O_CREAT|O_TRUNC,0644);//TODO fix the premission thing
 	if (fdOut < 0) {
 		printf("Error opening Output file: %s\n", strerror( errno));
-		return errno;
-	} else {
-		printf("File is opened. Descriptor %d\n", fdOut);
-	}
+		return -1;
+	} 
 
 	// Decide the size of the buffer
 	char *bufRD, *bufWR;
@@ -75,13 +73,13 @@ int main(int argc, char** argv) {
 	bufRD = (char*) malloc(bufSize * sizeof(*bufRD));
 	bufWR = (char*) malloc(bufSize * sizeof(*bufWR));
 	if (NULL == bufRD || NULL == bufWR) {
-		printf("allocation failed");
+		printf("allocation failed\n");
 		return -1;
 	}
-	long Cread = 0;
-	long CPrintable = 0;
-	ssize_t lenRD = 0;
-	ssize_t lenWR = 0;
+	long long Cread = 0;
+	long long CPrintable = 0;
+	long long lenRD = 0;
+	long long lenWR = 0;
 	int numReadable = 0;
 	i = 0;
 	for (; Cread < Creq; Cread += lenRD) {
@@ -95,13 +93,18 @@ int main(int argc, char** argv) {
 		if (lenRD == 0) {
 			if (Cread == 0) {
 				//the file is empty
-				printf("The file is empty");
+				printf("The input file is empty\n");
 				free(bufRD);
 				free(bufWR);
 				return -1;
 			}
 			else{//EOF
-				lseek(fdInput,0,SEEK_SET);//move to the beginning of the file
+				if(lseek(fdInput,0,SEEK_SET)<0){//move to the beginning of the file
+					printf("Error in lseek on the input file: %s\n", strerror(errno));
+					free(bufRD);
+					free(bufWR);
+					return -1;
+				}
 			}
 
 		}
@@ -111,6 +114,7 @@ int main(int argc, char** argv) {
 					bufWR[numReadable] = bufRD[i];
 					numReadable++;
 				}
+				//printf("the ascii num %d\n",bufRD[i] );
 				if(numReadable==bufSize-1){
 					lenWR = write(fdOut, bufWR, numReadable);
 					if (lenWR !=numReadable) {
@@ -119,8 +123,8 @@ int main(int argc, char** argv) {
 						free(bufWR);
 						return -1;
 					}
-				CPrintable+=numReadable;
-				numReadable=0;
+					CPrintable+=numReadable;
+					numReadable=0;
 				}
 			}
 		}
@@ -135,7 +139,10 @@ int main(int argc, char** argv) {
 			}
 		CPrintable+=numReadable;
 	}
-	printf("%li characters requested, %li characters read, %li are printable\n",Creq, Cread, CPrintable);
+	printf("%lld characters requested, %lld characters read, %lld are printable\n",Creq, Cread, CPrintable);
+	free(bufRD);
+	free(bufWR);
 	close(fdInput); // close file
 	close(fdOut); //close file
+	return 0;
 }
