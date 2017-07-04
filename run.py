b97faafa402b7f7d722e51951900fd0a879145bd
ronkashi <kashiron1@gmail.com>
2017-07-04 10:26:31 +0300

    with solution

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 import subprocess
 #FNULL = open(os.devnull, 'w')    #use this if you want to suppress output to stdout from the subprocess
 import csv
-with open('tests - Copy.csv') as f:
+i = 0
+with open('tests.csv') as f:
     reader = csv.reader(f)
     for row in reader:
         args = ''
@@ -9,6 +10,15 @@ with open('tests - Copy.csv') as f:
             args = args +" " + e
         #filename = "One	None	First	N/A	InMiddle	MoreThanAllowed	None	None	Visits	Lifetime	Matches	AllUsedUp	ServiceIsCovered	NPAR	DoesNotMatter	InLast	Header	Valid	Invalid	FALSE	Override	Default	belowRange	TRUE	FALSE	Unlimited	N/A	TRUE	NonHMO	TRUE	DIA"
         args = "hcbug.exe " + str(args)
-        p = subprocess.Popen(args,shell = True,stdout=subprocess.PIPE)
+        p = subprocess.Popen(args,shell = True,stdout=subprocess.PIPE,
+                             stderr = subprocess.PIPE)
         output, err = p.communicate()
-        print (output)
+        i = i + 1;
+        if i%100 == 0 :
+            print("iter num : %d",i)
+        if len(output) !=0  or len(err) != 0:
+            print (output)
+            print(err)
+            print (len(output))
+            print("argsssssss")
+            print(args)
