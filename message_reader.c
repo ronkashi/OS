36fb6270cbaf93ff227bb5142f2555ca29ee1710
ronkashi <kashiron1@gmail.com>
2017-07-11 09:55:34 +0300

    final v2

---
+++ message_reader.c
@@ -0,0 +1,62 @@
+#include "message_slot.h"    
+
+#include <fcntl.h>      /* open */ 
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BUFF_SIZE 128
+
+int main(int argc, char const *argv[])
+{
+    int file_desc, ret_val,ch_num;
+    char buff [BUFF_SIZE + 1];
+    if(argc != 2){
+        printf("Invalid parameters.\n");
+        printf("Please enter 1st arg : number of ch to read from\n");
+        return 0;
+    }
+    ch_num = atoi(argv[1]);
+    if (ch_num < 0 || ch_num > 3)
+    {
+        printf("the input %s is invalid pls enter 0 or 1 or 2 or 3\n",argv[1] );        
+        exit(-1);
+    }
+
+    file_desc = open("/dev/"DEVICE_FILE_NAME, O_RDWR);
+    if (file_desc < 0) {
+        printf ("Can't open device file: %s\n", DEVICE_FILE_NAME);
+        exit(-1);
+    }
+
+
+    ret_val = ioctl(file_desc, IOCTL_SET_CH, ch_num);
+
+    if (ret_val < 0) {
+        printf ("ioctl_set_channel failed:%d\n", ret_val);
+        close(file_desc);
+        exit(-1);
+    }
+
+    /////read /// 
+    ret_val = read(file_desc,buff,BUFF_SIZE);
+    if (ret_val < 0) {
+        printf ("read from file failed:%d\n", ret_val);
+        close(file_desc);
+        exit(-1);
+    }
+    buff[ret_val] = '\0';
+
+    if (close(file_desc) < 0) {
+        printf ("close file failed:%d\n", ret_val);
+        exit(-1);
+    }
+
+    printf("~~~~~~~~status message reader~~~~~~~\n");
+    printf("the message : %s\n was read fron : %d slot\n",buff,ch_num);
+    printf("the length that read is : %d\n",ret_val );
+    printf("~~~~end status message reader~~~~~~~\n");
+    return 0;
+}
---
+++ message_slot.mod.c
@@ -0,0 +1,37 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xb9e05320, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x6bc3fbc0, __VMLINUX_SYMBOL_STR(__unregister_chrdev) },
+	{ 0xb5dfcd8d, __VMLINUX_SYMBOL_STR(__register_chrdev) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0xda0cceae, __VMLINUX_SYMBOL_STR(kmem_cache_alloc) },
+	{ 0x2d203721, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0xc3aaf0a9, __VMLINUX_SYMBOL_STR(__put_user_1) },
+	{ 0x167e7f9d, __VMLINUX_SYMBOL_STR(__get_user_1) },
+	{ 0x680ec266, __VMLINUX_SYMBOL_STR(_raw_spin_lock_irqsave) },
+	{ 0x1916e38c, __VMLINUX_SYMBOL_STR(_raw_spin_unlock_irqrestore) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
