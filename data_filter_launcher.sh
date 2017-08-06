4ff10a026d69f2a7a6985d5750621cfad55867db
ronkashi <kashiron1@gmail.com>
2017-08-06 10:18:41 +0300

    Delete data_filter_launcher.sh

--- data_filter_launcher.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export DATA_SIZE=100B
-export INPUT_DATA=/dev/urandom
-export OUTPUT_DATA=/a/home/cc/students/math/ronkashi/Desktop/OS/b.txt
-
-./data_filter $DATA_SIZE $INPUT_DATA $OUTPUT_DATA
